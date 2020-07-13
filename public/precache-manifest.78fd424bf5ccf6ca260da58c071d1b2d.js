b6ba0e253b0da1667d447e3634c58afa9a3b2aa4
lewafa <saravana.rajkumar@gmail.com>
2020-07-13 19:47:49 +0200

    test commit

---
+++ public/precache-manifest.78fd424bf5ccf6ca260da58c071d1b2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f3d0fba86a8b5400de7f8952b59462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11137c6ca9e96bd8d21f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e1885fce5ae005d16ff1",
+    "url": "/static/js/2.1e2da53a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e2da53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11137c6ca9e96bd8d21f",
+    "url": "/static/js/main.b6c5d559.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
